51e4643a93d4b05236ae4ede6d6ff25cd92dc4e6
Heinrich Jiang <heinrich.jiang@gmail.com>
2013-11-09 15:15:54 -0500

    add line manipulations

--- client/js/translate.js
+++ client/js/translate.js
@@ -1,4 +1,37 @@
+Array.prototype.unique = function() {
+  var a = [];
+  var l = this.length;
+  for(var i=0; i<l; i++) {
+    for(var j=i+1; j<l; j++) { 
+      if (this[i] === this[j]) j = ++i; 
+    }
+    a.push(this[i]);
+  }
+  return a;
+};
+
 function translate(raw) {
+  // look for line variables
+  var hits = raw.match(/<([1-9]*[0-9])>/g);
+  if (hits) {
+    var replaced = raw.replace(/</g, "Var").replace(/>/g, "");
+    for( var i = 0; i < hits.length; i++)
+    { hits[i] = hits[i].match("<([1-9]*[0-9])>")[1]; }
+    hits = hits.unique();
+    var to_ret = replaced;
+    to_ret += " /. {";
+    for (var i = 0; i < hits.length; i++) {
+      to_ret += hits[i];
+      to_ret += "->";
+      to_ret += Prompt.results[hits[i-1]];
+      if (i != hits.length - 1) {
+        to_ret += ", ";
+      }
+    }
+    to_ret += "}";
+    return to_ret;
+  }
+
   // first look for the at
   var at_index = raw.search(" at ")
   if (at_index >= 0) {
